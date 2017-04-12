706cdeeec9d512c9b9ce2804e3539be546f637b4
woojgh <jamessalamonsen@gmail.com>
2017-04-11 20:24:27 -0700

    I added the min.js Query file and added my constructor function.  I also tried out giving my home item a url to an ID tage.

--- projectSources.js
+++ projectSources.js
@@ -1,1 +1,13 @@
-var projectSources = [];
+'use strict';
+
+var $projectLatest = $('projectLatest');
+var $projectPrevious = $('projectPrevious');
+var projectsArray = [];
+projectPrevious.append(projectsArray);
+
+function newProject(projectTitle, sourceName, sourceUrl){
+	this.projectTitle = projectTitle;
+	this.sourceName = sourceName;
+	this.sourceUrl = sourceUrl;
+	projectsArray.push(this);
+}
