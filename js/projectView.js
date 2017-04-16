60e1afd595c8c9022eb592b28177ba572075268c
woojgh <jamessalamonsen@gmail.com>
2017-04-15 21:35:20 -0700

    I got sooooo much done.

--- js/projectView.js
+++ js/projectView.js
@@ -21,17 +21,6 @@ projectView.setTeasers = function() {
   });
 };
 
-$('#interactive3d').interactive3d({ 
-  frames: 30,
-  cursor: "move", 
-  speed: 0, 
-  entrance: true, 
-  preloadImages: true,
-  touchSupport: true,
-  loading: "Loading..", 
-  autoPlay: false 
-  });
-
 
 $(document).ready(function() {
   projectView.handleMainNav();
