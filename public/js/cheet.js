ec8b0fd011c425a29be2f1a56b46062e1e8913fa
Woojgh <jamessalamonsen@gmail.com>
2017-04-21 09:38:10 -0700

    Added in day 07 stuff.

--- public/js/cheet.js
+++ public/js/cheet.js
@@ -1,8 +1,10 @@
 
-
-cheet('a', function () {
+cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
 	console.log('button pushed');
   $('#cheet').toggle();
+});
+cheet('i d d q d', function () {
+  alert('god mode enabled');
 });
 $(document).ready(function() {
 	cheet();
--- public/js/playGame.js
+++ public/js/playGame.js
@@ -10,7 +10,25 @@ var playKombat = function(){
 		$('.kombat-game').toggle();
 	});
 };
+var playUnicorn = function(){
+	$('#playunicorn').on('click', function(){
+		$('.unicorn-game').toggle();
+	});
+};
+var playCastle = function(){
+	$('#playcastle').on('click', function(){
+		$('.castle-game').toggle();
+	});
+};
+var playMario = function(){
+	$('#playmario').on('click', function(){
+		$('.mario-game').toggle();
+	});
+};
 $(document).ready(function(){
 	playLlama();
 	playKombat();
+	playUnicorn();
+	playCastle();
+	playMario();
 });
