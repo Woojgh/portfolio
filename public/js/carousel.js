e69b77b0219dbed2eddbb0514d56a7bca2ddeb91
Woojgh <jamessalamonsen@gmail.com>
2017-04-26 08:19:57 -0700

    I have added so much

--- public/js/carousel.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-      $('.count-down').slick({
-        setting-name: setting-value
-      });
-    });
--- public/js/rotatingText.js
+++ public/js/rotatingText.js
@@ -1,33 +1,47 @@
-CanvasRenderingContext2D.prototype.fillTextCircle = function(text,x,y,radius,startRotation) {
-   var numRadsPerLetter = 2*Math.PI / text.length;
-   this.save();
-   this.translate(x, y);
-   this.rotate(Math.PI*2/100*step++);  /* FOR CLOCK WISE*/
-   /*this.rotate(-Math.PI*2/100*step++);   FOR ANTICLOCK WISE*/
-   this.rotate(startRotation);
-// this.fillText(text, 0, -radius);
-// console.log(this);
-   for(var i=0;i<text.length;i++){
-      this.save();
-      this.rotate(i*numRadsPerLetter);
-
-      this.fillText(text[i],0,-radius);
-      this.restore();
-   }
-   this.restore();
-}
+'use strict';
+(function(module){
 
 var ctx = $('canvas').get(0).getContext('2d');
-
 var step = 0;
 
-function setInterval() {
-    ctx.clearRect(0, 0, 1000, 1000);
-    ctx.save();
-    ctx.fillTextCircle('MAKE ME DIZZY MAKE ME DIZZY MAKE ME DIZZY', 100, 100, 80, 0);
-    ctx.restore();
-};/*1000 = 1 second*/
+
+CanvasRenderingContext2D.prototype.fillTextCircle = function (text, x, y, radius, startRotation) {
+    var numRadsPerLetter = 2 * Math.PI / text.length;
+    this.save();
+    this.translate(x, y);
+    this.rotate(Math.PI * 5 / 100 * step++);  /* FOR CLOCK WISE*/
+    /*this.rotate(-Math.PI*2/100*step++);   FOR ANTICLOCK WISE*/
+    this.rotate(startRotation);
+    // console.log(this);
+    for (var i=0; i<text.length; i++){
+        this.save();
+        this.rotate(i*numRadsPerLetter);
+        this.fillText(text[i],0,-radius);
+        this.restore();
+    }
+    this.restore();
+};
+
+
 $(document).ready(function() {
-  setInterval();
-  // CanvasRenderingContext2D.fillTextCircle('text', x,y,radius,startRotation);
+  setInterval(function(){
+    ctx.clearRect(0, 0, 850, 850);
+    ctx.save();
+    ctx.fillStyle = ('style', 'color', 'white');
+    ctx.fillTextCircle('MAKE ME DIZZY MAKE ME ', 500, 400, 50, 0);
+    ctx.fillTextCircle('MAKE ME DIZZY MAKE ME DIZZY MAKE ME DIZZY ', 500, 400, 100, 0);
+    ctx.fillTextCircle('MAKE ME DIZZY MAKE ME DIZZY MAKE ME DIZZY ', 500, 400, 150, 0);
+    ctx.fillTextCircle('PoOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOp', 500, 400, 200, 0);
+    ctx.fillTextCircle('MAKE ME DIZZY MAKE ME DIZZY MAKE ME DIZZY ', 500, 400, 250, 0);        
+    ctx.fillTextCircle('MAKE ME DIZZY MAKE ME DIZZY MAKE ME DIZZY ', 500, 400, 300, 0);
+    ctx.fillTextCircle('MAKE ME DIZZY MAKE ME ', 500, 400, 50, 0);
+    ctx.fillTextCircle('MAKE ME DIZZY MAKE ME DIZZY MAKE ME DIZZY ', 500, 400, 100, 0);
+    ctx.fillTextCircle('MAKE ME DIZZY MAKE ME DIZZY MAKE ME DIZZY ', 500, 400, 150, 0);
+    ctx.fillTextCircle('POOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOp', 500, 400, 200, 0);
+    ctx.fillTextCircle('MAKE ME DIZZY MAKE ME DIZZY MAKE ME DIZZY ', 500, 400, 250, 0);        
+    ctx.fillTextCircle('MAKE ME DIZZY MAKE ME DIZZY MAKE ME DIZZY ', 500, 400, 300, 0);
+    ctx.restore();    
+  }, 90)
 });
+  
+})(window);
