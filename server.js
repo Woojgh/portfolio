e47ad9160987f6fc66930c306adfa02f551882fb
Woojgh <jamessalamonsen@gmail.com>
2018-04-04 16:03:47 -0700

    stash

--- server.js
+++ server.js
@@ -72,51 +72,51 @@ var apiRouter = express.Router();
 app.use('/api', apiRouter);
 
 var apiV1 = express.Router();
-apiRouter.use('/v1', apiV1);
+// apiRouter.use('/v1', apiV1);
 
-var playersApiV1 = express.Router();
-apiV1.use('/players', playersApiV1);
+// var playersApiV1 = express.Router();
+// apiV1.use('/players', playersApiV1);
 
-var boardsApiV1 = express.Router();
-apiV1.use('/leaderboards', boardsApiV1);
+// var boardsApiV1 = express.Router();
+// apiV1.use('/leaderboards', boardsApiV1);
 
-var PlayersController = require('./controllers/players');
-var pc = new PlayersController(playersApiV1);
-var BoardsController = require('./controllers/boards');
-var bc = new BoardsController(boardsApiV1);
-var ScoresController = require('./controllers/scores');
-var sc = new ScoresController(boardsApiV1);
+// var PlayersController = require('./controllers/players');
+// var pc = new PlayersController(playersApiV1);
+// var BoardsController = require('./controllers/boards');
+// var bc = new BoardsController(boardsApiV1);
+// var ScoresController = require('./controllers/scores');
+// var sc = new ScoresController(boardsApiV1);
 
-//  :D hardcoded for testing :D:D:D:D:D:D
-var PlayersService = require('./services/players');
-var p1 = PlayersService.addPlayer({firstName: 'Ben', lastName: 'Sparks', displayName: 'Warspawn'});
-var p2 = PlayersService.addPlayer({firstName: 'Joe', lastName: 'Blow', displayName: 'Joey558'});
-var p3 = PlayersService.addPlayer({firstName: 'Danny', lastName: 'Danger', displayName: 'DD83'});
-var BoardsService = require('./services/boards');
-var b1 = BoardsService.addBoard('Total Score', 1);
-var b2 = BoardsService.addBoard('Times Died', 0);
-var ScoresService = require('./services/scores');
-ScoresService.addScore(b1.id, p1.id, 3000);
-ScoresService.addScore(b1.id, p2.id, 2345);
-ScoresService.addScore(b1.id, p3.id, 15238);
-ScoresService.addScore(b2.id, p1.id, 33);
-ScoresService.addScore(b2.id, p2.id, 7);
-ScoresService.addScore(b2.id, p3.id, 67);
+// //  :D hardcoded for testing :D:D:D:D:D:D
+// var PlayersService = require('./services/players');
+// var p1 = PlayersService.addPlayer({firstName: 'Ben', lastName: 'Sparks', displayName: 'Warspawn'});
+// var p2 = PlayersService.addPlayer({firstName: 'Joe', lastName: 'Blow', displayName: 'Joey558'});
+// var p3 = PlayersService.addPlayer({firstName: 'Danny', lastName: 'Danger', displayName: 'DD83'});
+// var BoardsService = require('./services/boards');
+// var b1 = BoardsService.addBoard('Total Score', 1);
+// var b2 = BoardsService.addBoard('Times Died', 0);
+// var ScoresService = require('./services/scores');
+// ScoresService.addScore(b1.id, p1.id, 3000);
+// ScoresService.addScore(b1.id, p2.id, 2345);
+// ScoresService.addScore(b1.id, p3.id, 15238);
+// ScoresService.addScore(b2.id, p1.id, 33);
+// ScoresService.addScore(b2.id, p2.id, 7);
+// ScoresService.addScore(b2.id, p3.id, 67);
 
-var swaggerTools = require('swagger-tools');
-var options = {
-  swaggerUi: '/swagger.json',
-  controllers: './controllers'
-};
+// var swaggerTools = require('swagger-tools');
+// var options = {
+//   swaggerUi: '/swagger.json',
+//   controllers: './controllers'
+// };
 
-var swaggerDoc = require('./swagger.json');
+// var swaggerDoc = require('./swagger.json');
 
-swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
-    app.use(middleware.swaggerMetadata());
-    app.use(middleware.swaggerValidator());
-    app.use(middleware.swaggerRouter(options));
-    app.use(middleware.swaggerUi());
-});
+// swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+//     app.use(middleware.swaggerMetadata());
+//     app.use(middleware.swaggerValidator());
+//     app.use(middleware.swaggerRouter(options));
+//     app.use(middleware.swaggerUi());
+// });
 
 app.use(express.static('./public'));
 
